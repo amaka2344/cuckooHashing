bda0bad2a1e8769ef8c82fbfacc42c0763a81b70
amaka <amibaby411@gmail.com>
2024-07-09 12:05:26 +0100

    newcode

--- src/hashTableVisual.js
+++ src/hashTableVisual.js
@@ -77,14 +77,14 @@ const HashTableVisualizer = () => {
           <h2 className="text-2xl font-bold mb-2">Table 1</h2>
           {/* Display the contents of table1 */}
           {table1.map((value, index) => (
-            <div key={index} className="border text-white rounded p-6 font-medium bg-black">Slot {index}: {value}</div>
+            <div key={index} className="border text-black rounded p-6 font-medium border-purple-400">Slot {index}: {value}</div>
           ))}
         </div>
         <div>
           <h2 className="text-2xl font-bold mb-2">Table 2</h2>
           {/* Display the contents of table2 */}
           {table2.map((value, index) => (
-            <div key={index} className="border text-white rounded p-6 font-medium bg-black">Slot {index}: {value}</div>
+            <div key={index} className="border text-black rounded p-6 font-medium border-purple-400">Slot {index}: {value}</div>
           ))}
         </div>
       </div>
